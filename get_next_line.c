ba3822ac7bfe2dc2416253139caf478eede0d379
Xuna <romainchampliau@gmail.com>
2021-11-15 13:29:35 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rchampli <rchampli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 09:16:41 by rchampli          #+#    #+#             */
-/*   Updated: 2021/11/11 15:19:39 by rchampli         ###   ########.fr       */
+/*   Updated: 2021/11/11 16:09:34 by rchampli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,3 +104,4 @@ char	*get_next_line(int fd)
 	left_str = ft_new_left_str(left_str);
 	return (line);
 }
+ 
